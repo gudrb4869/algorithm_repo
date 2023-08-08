7aada883d774aa3b64a5f3180d4e3d59f25a5fb1
HyungKyu Park <93168480+gudrb4869@users.noreply.github.com>
2023-08-08 13:34:50 +0900

    [D4] Title: [S/W 문제해결 기본] 9일차 - 사칙연산 유효성 검사, Time: 111 ms, Memory: 20,852 KB -BaekjoonHub

--- SWEA/D4/1233. ［S／W 문제해결 기본］ 9일차 － 사칙연산 유효성 검사/［S／W 문제해결 기본］ 9일차 － 사칙연산 유효성 검사.java
+++ SWEA/D4/1233. ［S／W 문제해결 기본］ 9일차 － 사칙연산 유효성 검사/［S／W 문제해결 기본］ 9일차 － 사칙연산 유효성 검사.java
@@ -4,45 +4,67 @@ import java.util.StringTokenizer;
 
 /**
  * <pre>
- * 리프노드의 경우 노드번호와 데이터 두개의 값만 읽고
- * 리프노드가 아닌경우 노드번호, 데이터, 왼쪽자식노드번호,오른쪽자식노드번호 4개의 데이털르 받아오기때문에
- * 입력을 받을때 토큰의 길이가 4개면 연산자인지 검사하고 토큰의 길이가 2개면 숫자인지 검사하여 푼다.
+ * 리프노드인경우 숫자인지체크
+ * 리프노드아닌경우 왼쪽자식만있고 오른쪽자식없는경우 false리턴
+ * 리프노드아닌경우 왼쪽자식이올바른값이고 오른쪽자식이올바른값이고 자기자신이 연산자인 경우
  * </pre>
  * @author 박형규
+ * 메모리 19,104 KB
+ * 실행시간 114 ms
  *
  */
 public class Solution {
 
+	private static int n;
+	private static char[] tree;
+	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = null;
 		StringBuilder sb = new StringBuilder();
 		
 		for (int t = 1; t <= 10; t++) { // 10번 반복
-			int n = Integer.parseInt(br.readLine()); // 각 케이스의 트리가 갖는 정점의 총 수
-			boolean flag = true; // 계산 가능한지 여부
+			n = Integer.parseInt(br.readLine()); // 각 케이스의 트리가 갖는 정점의 총 수
+			tree = new char[n + 1];
 			
 			for (int i = 0; i < n; i++) { // n개의 정점 정보
 				st = new StringTokenizer(br.readLine());
-				st.nextToken(); // 정점 번호
+				int node = Integer.parseInt(st.nextToken()); // 정점 번호
 				char data = st.nextToken().charAt(0); // 값
-
-				// 리프노드가 아니면서 정점의 값이 숫자이거나, 리프노드인데 정점의 값이 숫자가 아니면
-				// 계산 불가능
-				if (st.hasMoreTokens() ^ data >= '0' ^ data <= '9') {
-					flag = false;
-				}
+				tree[node] = data; // 노드에 값저장
 			}
 			
 			sb.append("#").append(t).append(" ");
-			if (flag) {
-				sb.append("1\n");
-			} else {
-				sb.append("0\n");
+			if (dfs(1)) { // 유효하면
+				sb.append("1\n"); // 1
+			} else { // 유효하지 않으면
+				sb.append("0\n"); // 0
 			}
 			
 		}
-		System.out.print(sb); // 결과 출력
+		System.out.print(sb);
+	}
+
+	/**
+	 * 완전이진트리 탐색 메서드(왼쪽자식은없고 오른쪽자식이 있는 경우는 고려하지 않음)
+	 * 리프노드인 경우 숫자인지 체크
+	 * 리프노드가 아닌경우
+	 * 1) 왼쪽자식만 있는 경우 - 계산 불가하므로 유효하지 않음 false 반환
+	 * 2) 왼쪽,오른쪽 자식 둘다 있는 경우 - 왼쪽자식이 유효, 오른쪽 자식이 유효, 자기자신이 연산자인지 체크
+	 * @param node
+	 * @return
+	 */
+	private static boolean dfs(int node) {
+		int left = node * 2;
+		int right = node * 2  + 1;
+		
+		boolean isNumber = tree[node] >= '0' && tree[node] <= '9';
+		if (left > n && right > n) {
+			return isNumber;
+		} else if (left <= n && right <= n) {
+			return !isNumber && dfs(left) && dfs(right);
+		}
+		return false;
 	}
 
 }
