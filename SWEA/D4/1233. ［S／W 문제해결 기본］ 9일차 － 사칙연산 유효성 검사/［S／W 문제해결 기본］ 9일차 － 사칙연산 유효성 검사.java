07e649f0f5cdeef3e9ff8dad1a6c99fdf48a997f
HyungKyu Park <93168480+gudrb4869@users.noreply.github.com>
2023-08-08 11:57:36 +0900

    [D4] Title: [S/W 문제해결 기본] 9일차 - 사칙연산 유효성 검사, Time: 114 ms, Memory: 19,104 KB -BaekjoonHub

---
+++ SWEA/D4/1233. ［S／W 문제해결 기본］ 9일차 － 사칙연산 유효성 검사/［S／W 문제해결 기본］ 9일차 － 사칙연산 유효성 검사.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * <pre>
+ * 리프노드의 경우 노드번호와 데이터 두개의 값만 읽고
+ * 리프노드가 아닌경우 노드번호, 데이터, 왼쪽자식노드번호,오른쪽자식노드번호 4개의 데이털르 받아오기때문에
+ * 입력을 받을때 토큰의 길이가 4개면 연산자인지 검사하고 토큰의 길이가 2개면 숫자인지 검사하여 푼다.
+ * </pre>
+ * @author 박형규
+ *
+ */
+public class Solution {
+
+	private static int n; // 각 케이스의 트리가 갖는 정점의 총 수
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
+		StringBuilder sb = new StringBuilder();
+		
+		for (int t = 1; t <= 10; t++) { // 10번 반복
+			n = Integer.parseInt(br.readLine());
+			boolean flag = true;
+			
+			for (int i = 0; i < n; i++) {
+				st = new StringTokenizer(br.readLine());
+				st.nextToken();
+				char data = st.nextToken().charAt(0);
+				if (st.hasMoreTokens() ^ data >= '0' ^ data <= '9') {
+					flag = false;
+				}
+			}
+			
+			sb.append("#").append(t).append(" ");
+			if (flag) {
+				sb.append("1\n");
+			} else {
+				sb.append("0\n");
+			}
+			
+		}
+		System.out.print(sb);
+	}
+
+}
