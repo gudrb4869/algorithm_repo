447755d9d61c603bc61d34f30d1771b6485f6321
HyungKyu Park <93168480+gudrb4869@users.noreply.github.com>
2023-08-22 16:59:37 +0900

    [Gold V] Title: 암호 만들기, Time: 120 ms, Memory: 14168 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.java
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.java
@@ -5,70 +5,75 @@ import java.util.StringTokenizer;
 
 /**
  * <pre>
- * 암호는 L개의 알파벳 소문자로 구성. 최소 한개의 모음(a,e,i,o,u)과 최소 두개의 자음으로 구성되어 있음
- * 또한 암호는 항상 사전순으로 배열되어있다. abc는 가능성 있지만 bac는 그렇지 않음
- * C개의 문자가 주어졌을때 가능성 있는 암호들을 모두 구해야함
- * 사전순으로 배열되어있으므로 L개중에 C개를 순서를 고려하지 않고 뽑는 조합을 이용하면 될거같다.
+ * 자음 consonant
+ * 모음 vowel
+ * 최소 한개의 모음, 최소 두개의 자음 => 기저조건에서 검사하면 될듯
+ * 사전순으로 가능성있는 암호 => 조합을 이용하면 되보임
  * </pre>
  * @author 박형규
+ *
  */
 public class Main {
 
-	static int L, C, numbers[];
-	static char alphabet[];
+	static int L, C; // 암호를 구성하는 알파벳 개수, 암호로 사용가능한 문자의 개수
+	static char[] alphabet; // 알파벳을 저장할 1차원 배열
+	static char[] password; // 가능성있는 암호를 저장할 배열
 	static StringBuilder sb = new StringBuilder();
+	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		L = Integer.parseInt(st.nextToken()); // 암호를 구성하는 문자의 개수
-		C = Integer.parseInt(st.nextToken()); // 보안 시스템에 존재하는 문자의 개수
+		L = Integer.parseInt(st.nextToken()); // 암호를 구성하는 알파벳의 개수
+		C = Integer.parseInt(st.nextToken()); // 암호로 사용가능한 문자의 개수
 		
-		numbers = new int[L];
-		alphabet = new char[C];
+		alphabet = new char[C]; // 알파벳을 저장할 1차원 배열
+		password = new char[L];
 		
 		st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < C; i++) {
-			alphabet[i] = st.nextToken().charAt(0); // C개의 문자 저장
+			alphabet[i] = st.nextToken().charAt(0);
 		}
 		
-		Arrays.sort(alphabet); // 사전순으로 오름차순 정렬
-		combination(0, 0); // cCl 조합 경우의 수 완전탐색
+		Arrays.sort(alphabet); // 사전순으로 결과를 얻기 위해 사전순 정렬
 		
-		System.out.print(sb); // 사전식 가능성있는 암호를 모두 출력
+		dfs(0, 0); // 재귀 탐색
+		
+		System.out.print(sb); // 결과 출력
 	}
 
-	private static void combination(int cnt, int start) {
-
-		if (cnt == L) { // L개로 암호제작 끝난경우
-			StringBuilder temp = new StringBuilder();
-			for (int i = 0; i < L; i++) {
-				temp.append(alphabet[numbers[i]]); // 가능성 있는 암호 생성
-			}
-			String password = temp.toString(); // 가능성 있는 암호 후보
-			if (isPossible(password)) { // 가능성있는 암호라면
-				sb.append(password).append("\n"); // 스트링빌더에 저장
+	private static void dfs(int cnt, int start) {
+		
+		if (cnt == L) { // 기저조건
+			
+			if (isPossible()) { // 최소한개의 모음과 최소 두개의 자음으로 구성된 경우
+				sb.append(String.valueOf(password)).append("\n");
 			}
 			return;
 		}
 		
 		for (int i = start; i < C; i++) {
-			numbers[cnt] = i;
-			combination(cnt+1, i+1);
+			password[cnt] = alphabet[i];
+			dfs(cnt + 1, i + 1);
 		}
+		
 	}
 
-	private static boolean isPossible(String password) {
-		int consonant = 0, vowel = 0; //자음, 모음 개수
-		for (int i = 0; i < password.length(); i++) {
-			char current = password.charAt(i); // 현재 검사할 문자
-			if (current == 'a' || current == 'e' || current == 'i' || current == 'o' || current == 'u') { // 모음이면
-				vowel++; // 모음개수 1증가
-			} else { //자음이면
-				consonant++; // 자음개수 1증가
+	/**
+	 * 가능성 있는 암호인지 판단하는 메서드
+	 * @return
+	 */
+	private static boolean isPossible() {
+		int consonant = 0, vowel = 0; // 자음, 모음 개수
+		
+		for (int i = 0; i < L; i++) {
+			if (password[i] == 'a' || password[i] == 'e' || password[i] == 'i' || password[i] == 'o' || password[i] == 'u') {
+				vowel++;
+			} else {
+				consonant++;
 			}
 		}
-		return consonant >= 2 && vowel >= 1; // 최소2개의 자음이 있고 최소 1개의 모음이 있는 경우에만 true 리턴
+		return vowel >= 1 && consonant >= 2; // 최소 한개의 모음과 최소 두개의 자음으로 구성된 경우
 	}
 
 }
