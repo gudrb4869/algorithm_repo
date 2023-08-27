c448eddde8cf58e3ab033a93da6fbbec4d6022a3
HyungKyu Park <93168480+gudrb4869@users.noreply.github.com>
2023-08-27 23:06:41 +0900

    [Silver I] Title: 신입 사원, Time: 1876 ms, Memory: 302680 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.java
+++ 백준/Silver/1946. 신입 사원/신입 사원.java
@@ -1,13 +1,13 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.Comparator;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 /**
  * <pre>
- * 서류성적 순위기준 오름차순 정렬하고 면접성적 순위가 현재 순위보다 낮을경우 인원수 1증가시키고 현재순위를 갱신해주는 방식으로 풀음
+ * 서류성적 순위기준 오름차순으로 우선순위큐에 넣고 면접성적 순위가 현재 순위보다 낮을경우 인원수 1증가시키고 현재순위를 갱신해주는 방식으로 풀음
  * </pre>
  * @author 박형규
  *
@@ -24,26 +24,29 @@ public class Main {
 		for (int tc = 0; tc < T; tc++) {
 			int N = Integer.parseInt(br.readLine()); // 지원자의 숫자
 			
-			int[][] arr = new int[N][2];
-			for (int i = 0; i < N; i++) { // 각각의 지원자의 결과
-				st = new StringTokenizer(br.readLine());
-				arr[i][0] = Integer.parseInt(st.nextToken()); // 서류심사 성적
-				arr[i][1] = Integer.parseInt(st.nextToken()); // 면접 성적
-			}
-			
-			Arrays.sort(arr, new Comparator<int[]>() {
+			PriorityQueue<int[]> pq = new PriorityQueue<>(new Comparator<int[]>() {
 				@Override
 				public int compare(int[] a, int[] b) {
-					return a[0] - b[0]; // 서류 성적 오름차순으로 정렬
+					return a[0] - b[0]; // 서류성적 기준 오름차순 정렬
 				}
 			});
+			for (int i = 0; i < N; i++) { // 각각의 지원자의 결과
+				st = new StringTokenizer(br.readLine());
+				// 서류심사 성적, 면접 성적의 순위 입력받은 값을 우선순위큐에 삽입
+				pq.offer(new int[] {Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())});
+			}
 			
 			int answer = 1; // 선발할 수 있는 신입사원의 최대 인원 수
-			int current = arr[0][1];
-			for (int i = 0; i < N; i++) {
-				if (arr[i][1] < current) {
-					answer++;
-					current = arr[i][1];
+			int current = pq.poll()[1]; // 현재 면접 순위
+			while (!pq.isEmpty()) {
+				int[] info = pq.poll();
+				
+				if (info[1] < current) { // 지원자의 면접 순위가 현재 순위보다 낮으면
+					answer++; // 인원수 1증가시키고
+					current = info[1]; // 현재 순위 갱신
+				}
+				if (info[1] == 1) { // 만약 면접 1위이면 더이상 볼필요없음
+					break; // 중단
 				}
 			}
 			
