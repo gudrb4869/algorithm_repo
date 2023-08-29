221c01185685c94395612214b03a262f2467e18d
HyungKyu Park <93168480+gudrb4869@users.noreply.github.com>
2023-08-29 14:22:20 +0900

    [Silver III] Title: 1로 만들기, Time: 136 ms, Memory: 18176 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -11,21 +11,33 @@ import java.io.InputStreamReader;
  */
 public class Main {
 
+	static int[] memo;
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine()); // 1보다 크거나 같고 10^6보다 작거나 같은 정수
 		
-		int[] dp = new int[N + 1]; // dp table 초기화
-		for (int i = 2; i <= N; i++) {
-			dp[i] = dp[i - 1] + 1; // i를 1로만드는 횟수 = (i-1을 1로만드는 횟수+1)
-			if (i % 2 == 0) { // i가 2로 나누어 떨어지는 경우
-				dp[i] = Math.min(dp[i], dp[i / 2] + 1); // i/2를 1로만드는 횟수+1과 기존값중 최솟값으로 갱신
-			}
-			if (i % 3 == 0) { // i가 3으로 나누어 떨어지는 경우
-				dp[i] = Math.min(dp[i], dp[i / 3] + 1); // i/3을 1로만드는 횟수+1과 기존값중 최솟값으로 갱신
+		memo = new int[N + 1];
+		System.out.println(recursion(N));
+	}
+
+	private static int recursion(int n) {
+		if (n == 1) { // 1을 1로만드는 연산의 횟수는 0
+			return 0;
+		}
+		
+		if (memo[n] == 0) {
+			if (n % 6 == 0) { // 3과 2로 모두 나누어 떨어지는 경우
+				memo[n] = Math.min(recursion(n / 3), recursion(n / 2)) + 1; // 3으로나눈것과 2로 나눈것중 작은거 + 1
+			} else if (n % 3 == 0) { // 3으로 나누어 떨어지는 경우
+				memo[n] = Math.min(recursion(n / 3), recursion(n - 1)) + 1; // 3으로나눈것과 1뺀것중 작은거 + 1
+			} else if (n % 2 == 0) { // 2로 나누어 떨어지는 경우
+				memo[n] = Math.min(recursion(n / 2), recursion(n - 1)) + 1; // 2로나눈것과 1뺀것중 작은거 + 1
+			} else { // 3과 2로 나누어 떨어지지 않는 경우
+				memo[n] = recursion(n - 1) + 1; // 1뺀거 + 1
 			}
 		}
-		System.out.println(dp[N]); // N을 1로만드는 횟수의 최솟값
+		
+		return memo[n]; // 저장된 값 리턴
 	}
 
 }
