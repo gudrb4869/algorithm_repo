92a5660b229b9874983bc8b1bb998c022616ced1
HyungKyu Park <93168480+gudrb4869@users.noreply.github.com>
2023-09-17 00:01:12 +0900

    [Bronze II] Title: Hashing, Time: 128 ms, Memory: 14272 KB -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * <pre>
+ * a=1, b=2, ... z=26이라 하고
+ * 문자열의 각 항은 r=31을 인덱스만큼 거듭제곱한값을 곲하여 누적한다.
+ * 그러고 M=1,234,567,891로 나눈 나머지인 해시값을 구함
+ * </pre>
+ * @author 박형규
+ *
+ */
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); // 입력 스트림
+		
+		int L = Integer.parseInt(br.readLine()); // 문자열의 길이
+		char[] arr = br.readLine().toCharArray(); // 문자열
+		
+		long hash = 0;
+		int M = 1234567891;
+		for (int i = 0; i < L; i++) {
+			hash += (arr[i] - 'a' + 1) * Math.pow(31, i);
+			hash %= M;
+		}
+		System.out.println(hash);
+	}
+
+}
