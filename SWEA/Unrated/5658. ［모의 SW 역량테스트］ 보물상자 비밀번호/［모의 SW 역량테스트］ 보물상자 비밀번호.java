8fd43cc84e3a36dd92775e2a99b9c8633c172bc3
HyungKyu Park <93168480+gudrb4869@users.noreply.github.com>
2023-10-05 15:46:08 +0900

    [Unrated] Title: [모의 SW 역량테스트] 보물상자 비밀번호, Time: 117 ms, Memory: 21,048 KB -BaekjoonHub

--- SWEA/Unrated/5658. ［모의 SW 역량테스트］ 보물상자 비밀번호/［모의 SW 역량테스트］ 보물상자 비밀번호.java
+++ SWEA/Unrated/5658. ［모의 SW 역량테스트］ 보물상자 비밀번호/［모의 SW 역량테스트］ 보물상자 비밀번호.java
@@ -7,7 +7,10 @@ import java.util.TreeSet;
 
 /**
  * <pre>
- * 
+ * 인덱스 0부터 N/4 - 1까지 부분문자열을 기존문자열 뒤에 추가삽입함.
+ * 0부터 N-1까지 시작인덱스를 기준으로 N/4길이만큼의 부분문자열을 숫자로 전환한뒤에 트리셋에 저장(중복된 값을 거른 후, 내림차순으로 저장)
+ * K-1번만큼 pollFirst해줘서 1 ~ K-1번째 큰값들을 없앰
+ * first로 K번째 값 추출후 결과 출력
  * </pre>
  * @author 박형규
  *
@@ -33,33 +36,33 @@ public class Solution {
 				public int compare(Integer a, Integer b) {
 					return b - a;
 				}
-			});
+			}); // 트리셋 정렬조건 선언 
 			
-			String str = br.readLine();
-			str = str.concat(str.substring(0, N / 4 - 1));
+			String str = br.readLine(); // 16진수 숫자가 공백없이 주어짐
+			str = str.concat(str.substring(0, N / 4 - 1)); // 0부터 N/4-1까지의 부분문자열을 기존문자열 뒤에 삽입후 기존문자열을 바꿔치기함
 			
 			for (int i = 0; i < N; i++) {
-				set.add(strToInt(str.substring(i, i + N / 4)));
+				set.add(strToInt(str.substring(i, i + N / 4))); // 생성가능한 수들을 트리셋에 저장
 			}
 			
 			for (int i = 0; i < K - 1; i++) {
-				set.pollFirst();
+				set.pollFirst(); // 1 ~ K-1번째 수들을 버림
 			}
 			
-			sb.append("#").append(t).append(" ").append(set.first()).append("\n");
+			sb.append("#").append(t).append(" ").append(set.first()).append("\n"); // K번째로 큰수 출력
 		}
 		
 		System.out.print(sb);
 	}
 
 	private static int strToInt(String str) {
-		int value = 0;
+		int value = 0; // 문자열 -> 숫자로 변환한 결과값
 		for (int i = 0; i < str.length(); i++) {
-			char c = str.charAt(i);
-			value *= 16;
-			if (c >= '0' && c <= '9') {
+			char c = str.charAt(i); // i번째 문자
+			value *= 16; // 기존 값 한자리씩 올려줌
+			if (c >= '0' && c <= '9') { // 0~9인경우
 				value += c - '0';
-			} else if (c >= 'A' && c <= 'F') {
+			} else if (c >= 'A' && c <= 'F') { // A~F인 경우
 				value += 10 + c -'A';
 			}
 		}
