6300b49a27c1e5be6702529641b450e12bdd74d1
HyungKyu Park <93168480+gudrb4869@users.noreply.github.com>
2023-10-10 11:28:18 +0900

    [D4] Title: 창용 마을 무리의 개수, Time: 129 ms, Memory: 26,428 KB -BaekjoonHub

--- SWEA/D4/7465. 창용 마을 무리의 개수/창용 마을 무리의 개수.java
+++ SWEA/D4/7465. 창용 마을 무리의 개수/창용 마을 무리의 개수.java
@@ -1,62 +1,71 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
+/**
+ * <pre>
+ * 그래프 탐색 문제. 깊이 우선 탐색과 인접 리스트를 이용하여 문제 해결했다.
+ * </pre>
+ * @author 박형규
+ *
+ */
 public class Solution {
 
-	private static ArrayList<Integer>[] graph;
-	private static boolean[] visited;
+	static boolean[] visited;
+	static List<Integer>[] graph;
+	static int N, M;
 	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int T = Integer.parseInt(st.nextToken());
 		
-		for (int test_case = 1; test_case <= T; test_case++) {
-			st = new StringTokenizer(br.readLine());
-			int n = Integer.parseInt(st.nextToken());
-			int m = Integer.parseInt(st.nextToken());
+		int T = Integer.parseInt(br.readLine()); // 테스트 케이스의 수
+		
+		for (int t = 1; t <= T; t++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
 			
-			graph = new ArrayList[n + 1];
-			for (int i = 1; i < n + 1; i++) {
+			N = Integer.parseInt(st.nextToken()); // 창용 마을에 사는 사람의 수
+			M = Integer.parseInt(st.nextToken()); // 서로를 알고 있는 사람의 관계 수
+			
+			visited = new boolean[N + 1]; // 방문 배열
+			graph = new List[N + 1]; // 인접 리스트
+			for (int i = 1; i <= N; i++) {
 				graph[i] = new ArrayList<>();
 			}
-			for (int i = 0; i < m; i++) {
+			
+			for (int i = 0; i < M; i++) {
 				st = new StringTokenizer(br.readLine());
-				int a = Integer.parseInt(st.nextToken());
-				int b = Integer.parseInt(st.nextToken());
-				
-				graph[a].add(b);
-				graph[b].add(a);
+				int A = Integer.parseInt(st.nextToken());
+				int B = Integer.parseInt(st.nextToken());
+				graph[A].add(B);
+				graph[B].add(A);
 			}
 			
-			visited = new boolean[n + 1];
-			
-			int answer = 0;
-			for (int i = 1; i <= n; i++) {
+			int answer = 0; // 무리의 개수
+			for (int i = 1; i <= N; i++) {
 				if (!visited[i]) {
 					answer++;
 					dfs(i);
 				}
 			}
 			
-			sb.append("#" + test_case + " " + answer + "\n");
+			sb.append("#").append(t).append(" ").append(answer).append("\n");
 		}
+		
 		
 		System.out.print(sb);
 	}
-	
-	private static void dfs(int x) {
-		visited[x] = true;
-		
-		for (int j : graph[x]) {
-			if (!visited[j]) {
-				dfs(j);
+
+	// 깊이 우선 탐색
+	static void dfs(int cur) {
+		visited[cur] = true;
+		for (int next : graph[cur]) {
+			if (!visited[next]) {
+				dfs(next);
 			}
 		}
-		
 	}
 
 }
