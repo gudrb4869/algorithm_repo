3e1aa435b2d05d2b02bfcaac736fa472816f3597
HyungKyu Park <93168480+gudrb4869@users.noreply.github.com>
2023-10-10 10:37:30 +0900

    [Gold V] Title: 내려가기, Time: 372 ms, Memory: 41764 KB -BaekjoonHub

--- 백준/Gold/2096. 내려가기/내려가기.java
+++ 백준/Gold/2096. 내려가기/내려가기.java
@@ -4,7 +4,7 @@ import java.util.StringTokenizer;
 
 /**
  * <pre>
- * 다이나믹 프로그래밍으로 풀었다.
+ * 다이나믹 프로그래밍과 슬라이딩 윈도우로 풀었다.
  * </pre>
  * @author 박형규
  *
@@ -16,35 +16,32 @@ public class Main {
 		StringBuilder sb = new StringBuilder();
 		
 		int N = Integer.parseInt(br.readLine()); // 첫째 줄
-		int[][] arr = new int[N + 1][3];
-		
-		int[][] dpMin = new int[N + 1][3];
-		int[][] dpMax = new int[N + 1][3];
-		for (int i = 1; i <= N; i++) {
+		int[] dpMin = new int[3];
+		int[] dpMax = new int[3];
+		for (int i = 0; i < N; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			arr[i][0] = Integer.parseInt(st.nextToken());
-			arr[i][1] = Integer.parseInt(st.nextToken());
-			arr[i][2] = Integer.parseInt(st.nextToken());
-		}
-		
-		for (int i = 1; i <= N; i++) {
-			dpMin[i][0] = Math.min(dpMin[i - 1][0], dpMin[i - 1][1]) + arr[i][0];
-			dpMin[i][1] = Math.min(dpMin[i - 1][0], Math.min(dpMin[i - 1][1], dpMin[i - 1][2])) + arr[i][1];
-			dpMin[i][2] = Math.min(dpMin[i - 1][1], dpMin[i - 1][2]) + arr[i][2];
+			int X = Integer.parseInt(st.nextToken());
+			int Y = Integer.parseInt(st.nextToken());
+			int Z = Integer.parseInt(st.nextToken());
 			
-			dpMax[i][0] = Math.max(dpMax[i - 1][0], dpMax[i - 1][1]) + arr[i][0];
-			dpMax[i][1] = Math.max(dpMax[i - 1][0], Math.max(dpMax[i - 1][1], dpMax[i - 1][2])) + arr[i][1];
-			dpMax[i][2] = Math.max(dpMax[i - 1][1], dpMax[i - 1][2]) + arr[i][2];
+			if (i == 0) {
+				dpMin[0] = dpMax[0] = X;
+				dpMin[1] = dpMax[1] = Y;
+				dpMin[2] = dpMax[2] = Z;
+			} else {
+				int dpMin0 = dpMin[0], dpMin2 = dpMin[2];
+				dpMin[0] = Math.min(dpMin[0], dpMin[1]) + X;
+				dpMin[2] = Math.min(dpMin[1], dpMin[2]) + Z;
+				dpMin[1] = Math.min(dpMin[1], Math.min(dpMin0, dpMin2)) + Y;
+
+				int dpMax0 = dpMax[0], dpMax2 = dpMax[2];
+				dpMax[0] = Math.max(dpMax[0], dpMax[1]) + X;
+				dpMax[2] = Math.max(dpMax[1], dpMax[2]) + Z;
+				dpMax[1] = Math.max(dpMax[1], Math.max(dpMax0, dpMax2)) + Y;
+			}
 		}
 		
-		int minScore = 9999999;
-		int maxScore = 0;
-		for (int i = 0; i < 3; i++) {
-			minScore = Math.min(minScore, dpMin[N][i]);
-			maxScore = Math.max(maxScore, dpMax[N][i]);
-		}
-		
-		sb.append(maxScore).append(" ").append(minScore).append("\n");
+		sb.append(Math.max(dpMax[0], Math.max(dpMax[1], dpMax[2]))).append(" ").append(Math.min(dpMin[0], Math.min(dpMin[1], dpMin[2]))).append("\n");
 		
 		System.out.print(sb);
 	}
