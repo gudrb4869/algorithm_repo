e91affc16b79f48047c0ffb849ea78e4cbfd553c
HyungKyu Park <93168480+gudrb4869@users.noreply.github.com>
2023-10-11 16:05:40 +0900

    [D4] Title: 방향 전환, Time: 100 ms, Memory: 18,840 KB -BaekjoonHub

---
+++ SWEA/D4/8382. 방향 전환/방향 전환.java
@@ -0,0 +1,62 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * <pre>
+ * 두 x좌표 떨어진 거리 dx = |x1 - x2|
+ * 두 y좌표 떨어진 거리 dy = |y1 - y2|
+ * 
+ * 이동횟수
+ * r\c 0  1 2  3 4  5  6
+ *  0  0  1 4  5 8  9 ...
+ *  1  1  2 3  6 7 10 ...
+ *  2  4  3 4  5 8  9 ...
+ *  3  5  6 5  6 7 10 ...
+ *  4  8  7 8  7 8  9 ...
+ *  5  9 10 9 10 9 10 ...
+ *  6 ...................
+ *  
+ * 오른쪽위방향대각선을 차례대로 그었을때 r+c가 같은 것들을 한줄로 놓고보면 규칙성이있다.
+ * </pre>
+ * @author 박형규
+ *
+ */
+public class Solution {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int T = Integer.parseInt(br.readLine());
+		
+		for (int t = 1; t <= T; t++) {
+			
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			
+			int x1 = Integer.parseInt(st.nextToken());
+			int y1 = Integer.parseInt(st.nextToken());
+			int x2 = Integer.parseInt(st.nextToken());
+			int y2 = Integer.parseInt(st.nextToken());
+			
+			int dx = Math.abs(x1 - x2);
+			int dy = Math.abs(y1 - y2);
+			
+			int answer = dx + dy;
+			
+			int diff = Math.abs(dx - dy);
+			
+			int i = (dx + dy) % 2;
+			while (i < diff) {
+				answer += 2;
+				i += 2;
+			}
+			
+			sb.append("#").append(t).append(" ").append(answer).append("\n");
+		}
+		
+		System.out.print(sb);
+	}
+
+}
