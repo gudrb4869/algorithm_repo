341799ec2aad3174100e400178c76489fac27401
Hyeonggyu Park <93168480+gudrb4869@users.noreply.github.com>
2023-12-30 23:53:06 +0900

    [Silver II] Title: 쇠막대기, Time: 180 ms, Memory: 17240 KB -BaekjoonHub

--- 백준/Silver/10799. 쇠막대기/쇠막대기.java
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.java
@@ -10,35 +10,26 @@ import java.util.Stack;
  */
 public class Main {
 	
-	static class Data {
-		char c;
-		int idx;
-
-		public Data(char c, int idx) {
-			this.c = c;
-			this.idx = idx;
-		}
-	}
-
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		char[] str = br.readLine().toCharArray(); // 쇠막대기와 레이저의 배치를 나타내는 괄호 표현
 
-		Stack<Data> s = new Stack<>();
+		Stack<Integer> s = new Stack<>();
 		
 		int answer = 0; // 잘려진 조각의 총 개수
 		int index = 0; // 인덱스
+		
 		for (int i = 0; i < str.length; i++) {
 			if (str[i] == '(') { // (인경우
-				s.push(new Data(str[i], index++));
+				s.push(index++); // 쇠막대기의 시작위치 인덱스 스택에 삽입
 			} else { // )인경우
-				if (index - s.peek().idx == 1) { // 레이저인 경우
+				if (index - s.peek() == 1) { // 레이저인 경우
 					answer += s.size() - 1; // 쇠막대기의 개수만큼 총개수 누적
 				} else { // 막대기끝인 경우
 					answer++; // 1조각 누적
 				}
-				s.pop(); // 스택의 top 제거
+				s.pop();
 			}
 		}
 		
