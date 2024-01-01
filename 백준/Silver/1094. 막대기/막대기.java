e59100bbc3099002b4c20951927506154d0e9877
Hyeonggyu Park <93168480+gudrb4869@users.noreply.github.com>
2024-01-01 23:30:58 +0900

    [Silver V] Title: 막대기, Time: 124 ms, Memory: 14252 KB -BaekjoonHub

---
+++ 백준/Silver/1094. 막대기/막대기.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * <pre>
+ * 2024-01-01(월)
+ * </pre>
+ */
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int X = Integer.parseInt(br.readLine());
+		
+		String s = Integer.toBinaryString(X);
+		
+		int answer = 0;
+		
+		for (int i = 0; i < s.length(); i++) {
+			if (s.charAt(i) == '1') answer++;
+		}
+		
+		System.out.println(answer);
+		
+	}
+
+}
