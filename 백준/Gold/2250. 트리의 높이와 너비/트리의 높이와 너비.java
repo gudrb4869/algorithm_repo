d98751b5a0a4a932f347a6f61fa35b0afb73b412
Hyeonggyu Park <93168480+gudrb4869@users.noreply.github.com>
2024-03-03 23:20:20 +0900

    [Gold II] Title: 트리의 높이와 너비, Time: 320 ms, Memory: 23464 KB -BaekjoonHub

---
+++ 백준/Gold/2250. 트리의 높이와 너비/트리의 높이와 너비.java
@@ -0,0 +1,83 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.StringTokenizer;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
+public class Main {
+	
+	static int N, cnt = 1;
+	static List<Integer>[] graph;
+	static TreeMap<Integer, TreeSet<Integer>> treeMap = new TreeMap<>();
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		// 노드의 개수
+		N = Integer.parseInt(br.readLine());
+		
+		// 트리 구성
+		graph = new List[N + 1];
+		// 루트 노드 확인용
+		boolean[] visited = new boolean[N + 1];
+		
+		for (int i = 1; i <= N; i++) {
+			graph[i] = new ArrayList<>();
+		}
+		
+		for (int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			
+			int node = Integer.parseInt(st.nextToken());
+			
+			int left = Integer.parseInt(st.nextToken());
+			if (left != -1) visited[left] = true;
+			int right = Integer.parseInt(st.nextToken());
+			if (right != -1) visited[right] = true;
+			
+			graph[node].add(left);
+			graph[node].add(right);
+		}
+		
+		// 루트 노드의 번호
+		int root = 0;
+		for (int i = 1; i <= N; i++) {
+			if (!visited[i]) {
+				root = i;
+				break;
+			}
+		}
+		
+		// 중위 순회
+		inOrder(root, 1);
+		
+		// 트리의 높이들
+		Set<Integer> keys = treeMap.keySet();
+		
+		int level = 0, width = 0;
+		for (int key : keys) {
+			int w = treeMap.get(key).last() - treeMap.get(key).first() + 1;
+			if (w > width) {
+				level = key;
+				width = w;
+			}
+		}
+		
+		System.out.println(level + " " + width);
+	}
+	
+	static void inOrder(int node, int depth) {
+		if (node == -1)
+			return;
+		
+		inOrder(graph[node].get(0), depth + 1);
+		treeMap.computeIfAbsent(depth, k -> new TreeSet<>());
+		treeMap.get(depth).add(cnt++);
+		inOrder(graph[node].get(1), depth + 1);
+	}
+
+}
