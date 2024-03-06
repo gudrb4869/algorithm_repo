512ac3eebd1208790ba53339a1074ccdccad0323
Hyeonggyu Park <93168480+gudrb4869@users.noreply.github.com>
2024-03-06 23:31:52 +0900

    [Silver III] Title: N과 M (6), Time: 120 ms, Memory: 14304 KB -BaekjoonHub

--- 백준/Silver/15655. N과 M （6）/N과 M （6）.java
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.java
@@ -1,14 +1,11 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Main {
 
 	static int N, M, A[], R[];
-	static Set<String> set = new HashSet<>();
 	static StringBuilder sb = new StringBuilder();
 	
 	public static void main(String[] args) throws Exception {
@@ -36,15 +33,10 @@ public class Main {
 	static void combination(int start, int cnt) {
 		
 		if (cnt == M) {
-			StringBuilder sb2 = new StringBuilder();
 			for (int i = 0; i < M; i++) {
-				sb2.append(R[i]).append(" ");
+				sb.append(R[i]).append(" ");
 			}
-			String result = sb2.toString();
-			if (!set.contains(result)) {
-				set.add(result);
-				sb.append(result).append("\n");
-			}
+			sb.append("\n");
 			return;
 		}
 		
