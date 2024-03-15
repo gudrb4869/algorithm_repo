6a666a410e1a83e85681aa2ed5ecf1f82c18c8c2
Hyeonggyu Park <93168480+gudrb4869@users.noreply.github.com>
2024-03-16 01:20:07 +0900

    [Gold III] Title: 경사로, Time: 148 ms, Memory: 15052 KB -BaekjoonHub

--- 백준/Gold/14890. 경사로/경사로.java
+++ 백준/Gold/14890. 경사로/경사로.java
@@ -1,6 +1,5 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -9,16 +8,13 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		// 지도의 길이 
 		int N = Integer.parseInt(st.nextToken());
-		// 경사로의 길이
 		int L = Integer.parseInt(st.nextToken());
 		
 		int[][] A = new int[N][N];
 		
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
-			
 			for (int j = 0; j < N; j++) {
 				A[i][j] = Integer.parseInt(st.nextToken());
 			}
@@ -27,31 +23,28 @@ public class Main {
 		int answer = 0;
 	
 		for (int i = 0; i < N; i++) {
-			int up = 1;
-			int down = 0;
-			boolean isDown = false;
-			int prev = A[i][0];
-			boolean isPossible = true;
+			int up = 1, down = 0, prev = A[i][0];
+			boolean isDown = false, isPossible = true;
 			
 			for (int j = 1; j < N; j++) {
 				int cur = A[i][j];
+				
 				if (prev == cur) {
 					if (isDown) {
-						down++;
-						if (down == L) {
+						if (++down == L) {
 							isDown = false;
 							down = 0;
 						}
-					} else {
+					} else
 						up++;
-					}
+
 				} else if (cur - prev == 1) {
 					if (isDown || up < L) {
 						isPossible = false;
 						break;
-					} else {
-						up = 1;
 					}
+					up = 1;
+					
 				} else if (cur - prev == -1) {
 					if (isDown && down < L) {
 						isPossible = false;
@@ -69,41 +62,36 @@ public class Main {
 				prev = cur;
 			}
 			
-			if (isDown && down < L) {
+			if (isDown && down < L)
 				isPossible = false;
-			}
 			
-			if (isPossible) {
+			if (isPossible)
 				answer++;
-			}
 		}
 		
 		for (int j = 0; j < N; j++) {
-			int up = 1;
-			int down = 0;
-			boolean isDown = false;
-			int prev = A[0][j];
-			boolean isPossible = true;
+			int up = 1, down = 0, prev = A[0][j];
+			boolean isDown = false, isPossible = true;
 			
 			for (int i = 1; i < N; i++) {
 				int cur = A[i][j];
+				
 				if (prev == cur) {
 					if (isDown) {
-						down++;
-						if (down == L) {
+						if (++down == L) {
 							isDown = false;
 							down = 0;
 						}
-					} else {
+					} else
 						up++;
-					}
+
 				} else if (cur - prev == 1) {
 					if (isDown || up < L) {
 						isPossible = false;
 						break;
-					} else {
-						up = 1;
 					}
+					up = 1;
+					
 				} else if (cur - prev == -1) {
 					if (isDown && down < L) {
 						isPossible = false;
@@ -121,13 +109,11 @@ public class Main {
 				prev = cur;
 			}
 			
-			if (isDown && down < L) {
+			if (isDown && down < L)
 				isPossible = false;
-			}
 			
-			if (isPossible) {
+			if (isPossible)
 				answer++;
-			}
 		}
 		
 		System.out.println(answer);
